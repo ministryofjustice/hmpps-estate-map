117faaeff09be299d1a55b1281a58dce6b0a7ad0
Glen Mailer <glen@stainlessed.co.uk>
2017-12-09 22:08:41 +0000

    Exploring headers

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 const http = require('http');
 
 const express = require('express');
-const morgan = require('morgan');
 const expressEjsLayouts = require('express-ejs-layouts');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
 const bootstrapPackageJson = require('bootstrap/package.json');
 
 const azureExplorer = require('./azure-explorer');
 
+const dev = process.env.NODE_ENV != 'production';
 const config = {
-  dev: process.env.NODE_ENV != 'production',
+  dev,
   port: process.env.PORT || 2017,
+  sessionKeys: (process.env.SESSION_KEYS || (dev ? 'x' : '')).split(','),
   azure: {
     subscriptions: (process.env.ARM_SUBSCRIPTIONS || '').split(','),
+  },
+  auth: {
+    aad: process.env.WEBSITE_AUTH_AUTO_AAD,
+    clientID: process.env.WEBSITE_AUTH_CLIENT_ID,
+    clientSecret: process.env.WEBSITE_AUTH_CLIENT_SECRET,
   },
 };
 
@@ -19,13 +28,27 @@ const azure = azureExplorer(config.azure);
 
 const app = express();
 app.set('view engine', 'ejs');
-
+app.set('trust proxy', true);
 app.use(expressEjsLayouts);
 
 app.use(morgan(config.dev ? 'dev' : 'short'));
 
+app.use(cookieParser());
+app.use(cookieSession({
+  name: 'session',
+  keys: config.sessionKeys,
+
+  // Cookie Options
+  maxAge: 24 * 60 * 60 * 1000,
+  secure: !dev,
+}));
+
 app.get('/bootstrap.css', (req, res) => {
   res.sendFile(require.resolve(`bootstrap/${bootstrapPackageJson.style}`));
+});
+app.get('/logout', (req, res) => {
+  Object.keys(req.cookies).forEach(name => res.clearCookie(name));
+  res.redirect('/');
 });
 
 app.get('/', (req, res) => {
